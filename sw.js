a4cd582571c862be8daaf7287e6fd9b6f38f97de
douglasfdias <douglasdias.dfd@gmail.com>
2026-04-09 09:55:59 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'maximus-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['/maximus-agenda/', '/maximus-agenda/index.html', '/maximus-agenda/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
